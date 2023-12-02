1cb523f2f12215cf51068589606e7a6aead6f157
YueMelody <liuy80201@gmail.com>
2023-12-02 16:14:29 -0500

    a6

--- app.js
+++ app.js
@@ -2,15 +2,42 @@ import express from 'express'
 import Hello from "./hello.js"
 import Lab5 from './Lab5.js';
 import cors from "cors";
+import "dotenv/config";
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from './assignments/routes.js';
 import "dotenv/config";
 import session from "express-session";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use(express.json());
+
+app.use(cors(
+    {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+        }
+));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+    };
+    if (process.env.NODE_ENV !== "development") {
+        sessionOptions.proxy = true;
+        sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        };
+    }
+    app.use(
+    session(sessionOptions)
+    );
+
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
